58172911675de4ac640cf4ada285d8b66cf43330
BIGVEETECH <victorokoro944@gmail.com>
2025-07-22 04:33:02 -0700

    Add files via upload
    Attendance

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// js/firebase.js
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = firebase.firestore();
+
+// Set current event ID (you can access this from other scripts)
+const currentEventId = "Band_Rehearsal_2025";
